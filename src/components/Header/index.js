6cd7a266d533c70f5fff0542f9b63ebcd98222e4
achyuth <achyuth0910@gmail.com>
2021-11-23 09:52:27 +0000

    page_1

---
+++ src/components/Header/index.js
@@ -0,0 +1,72 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="nav-content">
+      <img
+        className="logo"
+        src="https://res.cloudinary.com/des7uyibo/image/upload/v1612513736/logo_1_riutsp.svg"
+        alt="logo"
+      />
+      <ul className="nav-menu">
+        <li>
+          <Link className="nav-link" to="/">
+            <img
+              className="social-network-img"
+              src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+              alt="Home"
+            />
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/about">
+            <img
+              className="social-network-img"
+              src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+              alt="About"
+            />
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/contact">
+            <img
+              className="social-network-img"
+              src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+              alt="Contact"
+            />
+          </Link>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/achyuth-reddy-b85256170/">
+            <img
+              className="social-network-img"
+              src="https://image.flaticon.com/icons/png/512/174/174857.png"
+              alt="Linked In"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/achyuth63?tab=repositories">
+            <img
+              className="social-network-img"
+              src="https://image.flaticon.com/icons/png/512/25/25231.png"
+              alt="Git Hub"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://learning.ccbp.in/">
+            <img
+              className="social-network-img"
+              src="https://res.cloudinary.com/des7uyibo/image/upload/v1605170763/5f383a4d6a6ea77270c43c01_Nxtwave_256x256_1_v1tqsr.png"
+              alt="nxtwave"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Header
